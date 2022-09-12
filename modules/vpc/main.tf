6e1a01938b65a3770628a3d21a7790a7a40e137f
soban <muhammad.soban@cheetay.pk>
2022-09-12 08:33:47 +0500

    add

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -98,7 +98,7 @@ resource "aws_route_table" "private" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    natnat_gateway_id  = aws_nat_gateway.nat.id
+    nat_gateway_id  = aws_nat_gateway.nat.id
   }
 
   
