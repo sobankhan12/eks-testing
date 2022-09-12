ad48e6abf3a9bca49691a44b4f5f4f39e6a67b12
soban <muhammad.soban@cheetay.pk>
2022-09-12 11:13:59 +0500

    add

--- main.tf
+++ main.tf
@@ -15,6 +15,12 @@ terraform {
       version = "~> 4.2"
     }
   }
+  backend "remote" {
+    organization = "soban252"
+
+    workspaces {
+      name = "eks-testing"
+    }
 
 }
 provider "aws" {
